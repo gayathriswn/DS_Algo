3bf288768dd55e7d08c23ab3e90ac0cb5c22c4ec
gayat <gayat@DESKTOP-FAIQAUP.lan>
2023-03-30 16:25:14 -0500

    commit 3/30

--- src/test/java/Runner/TestRunner.java
+++ src/test/java/Runner/TestRunner.java
@@ -1,6 +1,8 @@
 package Runner;
 //import org.junit.runner.RunWith;
 import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 //import io.cucumber.junit.Cucumber;
@@ -27,6 +29,7 @@ import io.cucumber.testng.CucumberOptions;
 	    		  */  
 	    //location of feature files
 		glue= "StepDefinition") //location of step definition files
+
 
 
  public class TestRunner extends AbstractTestNGCucumberTests
